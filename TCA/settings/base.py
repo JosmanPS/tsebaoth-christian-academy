bf589826ef7d655c8c95a8f160a2cf36496201f3
Josman Proudinat <jmps2812@gmail.com>
2017-06-27 14:07:42 -0500

    Base de datos producción

--- TCA/settings/base.py
+++ TCA/settings/base.py
@@ -97,7 +97,7 @@ if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine'):
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
-            'HOST': '/cloudsql/tsebaoth-academy:us-central1:tca-db',
+            'HOST': '/cloudsql/tca-platform:tca-db',
             'NAME': 'tca',
             'USER': 'root'
         }
